3af6d7d7b3d569d40b33dc76eac3e9561fedc76b
Elakkiya <celakkiyaece@gmail.com>
2024-07-30 08:29:33 +0530

    added programs

--- string/zigzag_pattern_matrix.c
+++ string/zigzag_pattern_matrix.c
@@ -23,11 +23,19 @@ void pattern(char *str, int n, int rows)
         cnt = 0;
         while (j < n)
         {
-            if ()
+            if (col % (rows - 1) == 0)
             {
+                printf("%c", str[j]);
+                j += (2 * rows) - 2 * (i + 1);
+                if ((2 * rows) - 2 * (i + 1) == 0)
+                {
+                    j += 2 * rows - 2;
+                }
             }
-            else if ()
+            else if (col % (rows - 1) == (rows - 1) - i)
             {
+                printf("%c", str[j]);
+                j += 2 * i;
             }
             else
             {
